7b48deb02a4898a519ea81a1a32be72d4f2564aa
michgur <michael@thegur.net>
2022-10-12 13:26:54 +0300

    installed express

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/', async (req, res) => {
   // Use basic logger without HTTP request info
   logger.info({
     logField: 'custom-entry',
-    arbitraryField: 'custom-entry'
+    arbitraryField: 'custom-entry',
   }); // Example of structured logging
   // Use request-based logger with log correlation
 
